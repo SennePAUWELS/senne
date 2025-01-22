f675846f64378e7e5e5b433d223ca1f85f9caad7
SennePAUWELS <sennpauw@student.arteveldhs.be>
2025-01-22 19:57:00 +0100

    w

--- src/js/nav.js
+++ src/js/nav.js
@@ -9,6 +9,17 @@ const navmenu = function () {
       overlay.style.display = 'none';
     }
   });
+  const arrnavlinks = document.querySelectorAll('.js-navlink');
+ for(navlink of arrnavlinks){
+    navlink.addEventListener('click', function (e){
+        navmenu.checked = 0;
+        if (navmenu.checked) {
+            overlay.style.display = 'block';
+          } else {
+            overlay.style.display = 'none';
+          }
+    })
+ }
 };
 
 document.addEventListener('DOMContentLoaded', navmenu);
